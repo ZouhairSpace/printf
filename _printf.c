f37b14cd225b1767fef8f7bab7d6643a5fb42035
ZouhairSpace <jeddi .zouhair@gmail.com>
2023-04-27 09:57:34 -0700

    t0

--- _printf.c
+++ _printf.c
@@ -1,43 +1,41 @@
 #include "main.h"
 
-void print_buffer(char buffer[], int *buff_ind);
-
+void flush_buffer(char buffer[], int *buff_index);
 /**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
+ * _printf - custom printf function
+ * @format: format string
+ * Return: number of printed characters
  */
 int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
+	int flags, width, precision, size, buff_index = 0;
+	va_list arg_list;
 	char buffer[BUFF_SIZE];
 
 	if (format == NULL)
 		return (-1);
 
-	va_start(list, format);
+	va_start(arg_list, format);
 
 	for (i = 0; format && format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
 		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
+			buffer[buff_index++] = format[i];
+			if (buff_index == BUFF_SIZE)
+				flush_buffer(buffer, &buff_index);
 			printed_chars++;
 		}
 		else
 		{
-			print_buffer(buffer, &buff_ind);
+			flush_buffer(buffer, &buff_index);
 			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
+			width = get_width(format, &i, arg_list);
+			precision = get_precision(format, &i, arg_list);
 			size = get_size(format, &i);
 			++i;
-			printed = handle_print(format, &i, list, buffer,
+			printed = handle_print(format, &i, arg_list, buffer,
 				flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
@@ -45,22 +43,23 @@ int _printf(const char *format, ...)
 		}
 	}
 
-	print_buffer(buffer, &buff_ind);
+	flush_buffer(buffer, &buff_index);
 
-	va_end(list);
+	va_end(arg_list);
 
 	return (printed_chars);
 }
 
 /**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
- */
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
+ * flush_buffer - writes the contents of the buffer
+ * @buffer: character array buffer to be written
+ * @buff_index: index at which to add next char, represents the length
+*/
 
-	*buff_ind = 0;
+void flush_buffer(char buffer[], int *buff_index)
+{
+	if (*buff_index > 0)
+		write(1, &buffer[0], *buff_index);
+
+	*buff_index = 0;
 }
